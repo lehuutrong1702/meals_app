99762ef5be556b031afff07213f148328915866b
trong <lehuutrong141@gmail.com>
2024-09-15 09:43:42 +0700

    add grid view

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,29 @@
 import 'package:flutter/material.dart';
 
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meals_app/cateogories.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+    brightness: Brightness.dark,
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      home: CategoriesScreen(),
+    );
+  }
+}
